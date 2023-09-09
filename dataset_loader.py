f971bda6b6dc49611fcf31ed76173698cd0b477f
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-09-09 16:14:09 +0530

    fix(dataset_loader): changing implementation to make a generator without loading all of the data in one go due to memory issues

--- dataset_loader.py
+++ dataset_loader.py
@@ -17,39 +17,42 @@ class MOTDataset(Dataset):
         self.img_shape = resized_img_shape
         self.orig_img_shape = orig_img_shape
 
-        self.img_data_full, self.bboxes_all, self.classes_all = self.load_data()
+        # self.img_data_full, self.bboxes_all, self.classes_all = self.load_data()
 
     def __len__(self):
         return self.img_data_full.size(dim=0)
     
     def __getitem__(self, index: int):
-        return self.img_data_full[index], self.bboxes_all[index], self.classes_all[index]
+        # return self.img_data_full[index], self.bboxes_all[index], self.classes_all[index]
+        data = self.load_data(index)
     
-    def load_data(self) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def load_data(self, index) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         
-        img_data_full = []
+        # img_data_full = []
 
-        bboxes_all, classes_all, img_paths = parse_annotations(
-            annotation_path=self.annotation_path,
-            image_dir=self.img_dir,
-            resized_img_size=self.img_shape,
-            original_img_shape=self.orig_img_shape)
+        # bboxes_all, classes_all, img_paths = parse_annotations(
+        #     annotation_path=self.annotation_path,
+        #     image_dir=self.img_dir,
+        #     resized_img_size=self.img_shape,
+        #     original_img_shape=self.orig_img_shape)
         
-        print("Loading image data from file paths...")
-        for i, img_path in enumerate(tqdm(img_paths)):
-            
-            # Check if image path is valid, otherwise skip
-            if (not img_path) or (not os.path.exists(img_path)):
-                continue
-                
-            # read and resize image
-            img = imread(img_path)
-            img = resize(img, self.img_shape)
-            
-            # convert image to torch tensor and reshape it so channels come first
-            img_tensor = torch.from_numpy(img).permute(2, 0, 1)
-            img_data_full.append(img_tensor)
+        # print("Loading image data from file paths...")
+        # for i, img_path in enumerate(tqdm(img_paths)):
+        
+        img_path = ids[index]
 
-        img_data_stacked = torch.stack(self.img_data_full, dim=0)
+        # Check if image path is valid, otherwise skip
+        if (not img_path) or (not os.path.exists(img_path)):
+            continue
+            
+        # read and resize image
+        img = imread(img_path)
+        img = resize(img, self.img_shape)
+        
+        # convert image to torch tensor and reshape it so channels come first
+        img_tensor = torch.from_numpy(img).permute(2, 0, 1)
+        # img_data_full.append(img_tensor)
+
+        # img_data_stacked = torch.stack(self.img_data_full, dim=0)
 
         return img_data_stacked.to(dtype=torch.float32), bboxes_all, classes_all
