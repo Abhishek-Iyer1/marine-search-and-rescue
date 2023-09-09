f44645056e25fe0a4c2b284472372968503eb226
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-09-09 16:13:20 +0530

    fix(parse_annotations): changed parse annotations to load and create a dictionary with all image paths and all bboxes and classes

--- utils.py
+++ utils.py
@@ -1,42 +1,50 @@
 import json
 import os
 import torch
+import cv2
+import matplotlib.pyplot as plt
 
-from tqdm import tqdm 
+from tqdm import tqdm
+from skimage.io import imread
+from skimage.transform import resize
 
-def parse_annotations(annotation_path: str, image_dir: str, resized_img_size: tuple[int, int], original_img_shape: tuple[int, int, int]):
-    img_h, img_w = resized_img_size
-    orig_img_h, orig_img_w, _ = original_img_shape
+def parse_annotations(annotation_path: str, image_dir: str):
 
-    w_scale = img_w/orig_img_w
-    h_scale = img_h/orig_img_h
+    partition = {
+        "all": [],
+        "train": [],
+        "validation": [],
+        "test": []
+    }
 
-    img_paths = []
-    bboxes_all = []
-    classes_all = []
+    labels = {
+        "all": [],
+        "train": [],
+        "validation": [],
+        "test": []
+    }
 
     # Opening annotations JSON file and loading data
     with open(annotation_path) as json_file:
         annotations: dict = json.load(json_file)
 
     print("Checking for annotations, collecting file paths...")
-    for file in tqdm(os.listdir(image_dir)):
-        image_path = os.path.join(image_dir, file)
-        img_paths.append(image_path)
+    sorted_img_dir = sorted(os.listdir(image_dir), key= lambda y: int(y.split(".")[0]))
+    sorted_annotations = sorted(annotations["annotations"], key= lambda y: int(y["image_id"]))
+
+    for file in tqdm(sorted_img_dir):
+        partition["all"].append(os.path.join(image_dir, file))
         img_number = file.split(".")[0]
 
-        groundtruth_bboxes = []
-        groundtruth_categories = []
+        temp_labels = {}
 
-        for ann in annotations["annotations"]:
-            if ann["image_id"] == img_number:
+        for ann in sorted_annotations:
+            if int(ann["image_id"]) == int(img_number):
                 x1, y1, w, h = ann["bbox"]
                 x2 = x1 + w
                 y2 = y1 + h
 
                 bbox = torch.Tensor([x1, y1, x2, y2])
-                bbox[[0, 2]] *= w_scale
-                bbox[[1, 3]] *= h_scale
 
                 category_id = ann["category_id"]
 
