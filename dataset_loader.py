ce6b7166b5286e55b7d6e643a1806d3a2a5e012d
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-09-11 17:16:24 +0530

    fix(dataset_loader): cleaned up the implementation and removed unused functions and logging statements

--- dataset_loader.py
+++ dataset_loader.py
@@ -39,14 +39,7 @@ class MOTDataset(Dataset):
         
             # convert image to torch tensor and reshape it so channels come first
             img_tensor = torch.from_numpy(img).permute(2, 0, 1)
-            # img_data_stacked = torch.stack(self.img_data_full, dim=0)
             class_label = torch.IntTensor(self.categories[index - 1])
             bbox_label = torch.IntTensor(self.bboxes[index - 1])
-
-            # print(f"Class Labels: {class_label}\nBbox Labels: {bbox_label}\nImg Size: {img_tensor.size()}\nClass Labels Size: {class_label.size()}\nBbox Labels Size: {bbox_label.size()}")
-            # class_labels_padded = pad_sequence(class_label, batch_first=True)
-            # bbox_labels_padded = pad_sequence(bbox_label, batch_first=True)
-            # print(f"Class Labels: {class_label}\nBbox Labels: {bbox_label}\nImg Path: {img_path}\nClass Labels Padded Size: {class_label.size()}\nBbox Labels Padded Size: {bbox_label.size()}")
-
 
         return img_tensor, class_label, bbox_label
