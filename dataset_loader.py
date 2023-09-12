d475a686eb97bdc07a2f72d0463ca6c7b67331a4
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-09-12 20:33:22 +0530

    fix(resize): resizing both images and their labels to be able to load and train on local gpu

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import numpy as np
 
 from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
@@ -34,12 +35,20 @@ class MOTDataset(Dataset):
         if (img_path) and (os.path.exists(img_path)):
             
             # read and resize image
-            img = imread(img_path)
-            # img = resize(img, self.img_shape)
+            img = imread(img_path) / 255
+            img = resize(img, self.img_shape)
         
             # convert image to torch tensor and reshape it so channels come first
-            img_tensor = torch.from_numpy(img).permute(2, 0, 1)
+            img_tensor = torch.FloatTensor(img).permute(2, 0, 1)
             class_label = torch.IntTensor(self.categories[index - 1])
-            bbox_label = torch.IntTensor(self.bboxes[index - 1])
+            bbox_label = self.bboxes[index - 1]
+            bbox_label = torch.IntTensor(bbox_label)
+            for i in range(0, len(bbox_label)):
+                # print(f"Before: {bbox_label[i]}, Original Shape: {self.orig_img_shape[0], self.orig_img_shape[1]}, Resized Shape: {self.img_shape[0], self.img_shape[1]}")
+                bbox_label[i][0] = bbox_label[i][0] * (self.img_shape[1]/self.orig_img_shape[1])
+                bbox_label[i][1] = bbox_label[i][1] * (self.img_shape[0]/self.orig_img_shape[0])
+                bbox_label[i][2] = bbox_label[i][2] * (self.img_shape[1]/self.orig_img_shape[1])
+                bbox_label[i][3] = bbox_label[i][3] * (self.img_shape[0]/self.orig_img_shape[0])
+                # print(f"After: {bbox_label[i]}")
 
         return img_tensor, class_label, bbox_label
