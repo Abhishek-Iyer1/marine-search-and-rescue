ce8061ffc78bd7d252b2cc39af3621234cc3e906
Abhishek Iyer <iyer.abhishek18@gmail.com>
2023-09-23 01:29:51 +0530

    fix(train): modified valid loss method during training

--- train.py
+++ train.py
@@ -62,10 +62,12 @@ def training_loop(model: TwoStageDetector,  n_epochs: int, learning_rate: float,
     
     for i in tqdm(range(n_epochs)):
         
-        # Begin training loop
+        # Begin training loop ------------------------------------------------------------------------
 
         # Initialize training loss
-        train_loss = 0
+        train_loss = 0      
+        # Set model to train mode
+        model.train()
 
         for img_batch, gt_classes_batch, gt_bboxes_batch in tqdm(train_dataloader):
             
@@ -79,7 +81,7 @@ def training_loop(model: TwoStageDetector,  n_epochs: int, learning_rate: float,
             # Forward pass and find loss
             batch_loss = model.forward(img_batch, gt_bboxes_batch, gt_classes_batch)
             # print(batch_loss)
-
+            
             # Backpropagation
             batch_loss.backward()
 
@@ -96,19 +98,40 @@ def training_loop(model: TwoStageDetector,  n_epochs: int, learning_rate: float,
         model.eval()
 
         for val_images, val_classes, val_bboxes in val_dataloader:
+            
+            val_images: torch.Tensor = val_images.to(config.DEVICE)
+            val_bboxes: torch.Tensor = val_bboxes.to(config.DEVICE)
+            val_classes: torch.Tensor = val_classes.to(config.DEVICE)
+            
             # Forward Pass
-            target = model.inference(val_images.to(config.DEVICE))
+            proposals_final, conf_scores_final, classes_final, cls_scores = model.inference(val_images, config.CONF_THRESH, config.NMS_THRESH)
             # Find the Loss
-            loss = model.forward(img_batch, gt_bboxes_batch, gt_classes_batch)
-             # Calculate Loss
+            loss = model.forward(val_images, val_bboxes, val_classes)
+
+            # Alternate loss
+            # print(f"Proposals: {proposals_final[0]}, Val Bboxes: {val_bboxes}, Classes_final : {classes_final}, Val classes: {val_classes}")
+
+            bbox_loss = calc_giou_loss(proposals_final, val_bboxes)
+            # class_loss = F.cross_entropy(cls_scores, val_classes.long())
+
+            # print(f"Bbox Loss GIOU: {bbox_loss}, Class Loss Cross Entropy: {class_loss}")
+            # IOU LOSS OVER PROPOSALS AND GT BBOXES
+            # get_req_anchors()
+            # cls_loss = calc_cls_loss(conf_scores_pos, conf_scores_neg, batch_size)
+            # reg_loss = calc_bbox_reg_loss(GT_offsets, offsets_pos, batch_size)
+            valid_loss = bbox_loss * config.BBOX_WEIGHT + loss * config.CLASS_WEIGHT
+            # Calculate Loss
             valid_loss += loss.item()
         
+        scheduler.step(valid_loss)
+
         print(f"Epoch Valid Loss: {valid_loss}")
         
         # Keep track of loss for every epoch
         loss_list["train"].append(train_loss)
         loss_list["val"].append(valid_loss)
         
+        # If epoch loss better than previous minimum, update loss, save model
         if min_valid_loss > valid_loss:
             
             # Update the new minimum
@@ -117,6 +140,8 @@ def training_loop(model: TwoStageDetector,  n_epochs: int, learning_rate: float,
             # Saving State Dict
             print(f"Saving model to path: {checkpoint_path}")
             torch.save(model.state_dict(), checkpoint_path)
+
+        # End training loop --------------------------------------------------------------------------
     
     return loss_list
 
